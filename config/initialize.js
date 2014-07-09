6e903643fee4fc0bc92805b9249e2eda786dfb91
John Lee <zintarweb@gmail.com>
2014-07-09 13:46:30 -0700

    db settings updated

--- config/initialize.js
+++ config/initialize.js
@@ -1,8 +1,15 @@
-DBoptions = {
+DBoptions_orig = {
     host     : 'localhost',
     user     : 'root',
     password : 'lampshade!',
     database : 'dourhour'
+};
+
+DBoptions = {
+    host     : 'localhost',
+    user     : 'b17c0d049c773a',
+    password : '075d6dd9',
+    database : 'heroku_dd220e7b2a3aa7f'
 };
 
 server = {
