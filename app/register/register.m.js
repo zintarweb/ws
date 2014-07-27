b13238cd869c7c4b88997d062eb9a275e18fbef4
zintarweb <zintarweb@gmail.com>
2014-07-27 16:32:44 -0700

    registration update

--- app/register/register.m.js
+++ app/register/register.m.js
@@ -29,17 +29,18 @@ mData.prototype.registerUser = function(cb, cb_arg, req) {
 		Phone: params.phone
 	}
 
-sql = 'select UserID from tUsers where email = ? '	
-conn.query(sql, data, function(err, rows, fields) {
-	if (err) throw err;
-	if (!rows) {
-		sql = 'insert into tUsers set ? '
-		conn.query(sql, data, function(err, rows, fields) {
-			if (err) throw err;
-			self.all = rows;
-			cb(cb_arg);
-		});
-	}
+	sql = 'select UserID from tUsers where email = ? '	
+	conn.query(sql, data, function(err, rows, fields) {
+		if (err) throw err;
+		if (!rows) {
+			sql = 'insert into tUsers set ? '
+			conn.query(sql, data, function(err, rows, fields) {
+				if (err) throw err;
+				self.all = rows;
+				cb(cb_arg);
+			})
+		};
+	});
 };
 
 mData.prototype.getByID = function(cb, cb_arg) {
