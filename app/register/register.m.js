acd6f940dd565a631ecdac2065150602f0ca6eb5
zintarweb <zintarweb@gmail.com>
2014-07-27 16:24:47 -0700

    registration update

--- app/register/register.m.js
+++ app/register/register.m.js
@@ -21,20 +21,25 @@ mData.prototype.registerUser = function(cb, cb_arg, req) {
 		UserType: 1,
 		CreationDate: '2014-04-01 13:00:00',
 		LastModified: '2014-04-01 13:00:00',
-		Fname: 'test',
-		Lname: 'test',
+		Fname: params.fname,
+		Lname: params.lname,
 		Email : params.email,
 		Login: params.email,
 		Secret: params.secret,
-		Phone: '123'
+		Phone: params.phone
 	}
 
-	sql = 'insert into tUsers set ? '
-	conn.query(sql, data, function(err, rows, fields) {
-		if (err) throw err;
-		self.all = rows;
-		cb(cb_arg);
-	});
+sql = 'select UserID from tUsers where email = ? '	
+conn.query(sql, data, function(err, rows, fields) {
+	if (err) throw err;
+	if (!rows) {
+		sql = 'insert into tUsers set ? '
+		conn.query(sql, data, function(err, rows, fields) {
+			if (err) throw err;
+			self.all = rows;
+			cb(cb_arg);
+		});
+	}
 };
 
 mData.prototype.getByID = function(cb, cb_arg) {
