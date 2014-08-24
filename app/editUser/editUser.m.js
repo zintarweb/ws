f13ce8880e95ec93b027df104b3ed726e855a8ba
John Lee <zintarweb@gmail.com>
2014-08-24 15:25:46 -0700

    edituser

--- app/editUser/editUser.m.js
+++ app/editUser/editUser.m.js
@@ -19,9 +19,6 @@ mData.prototype.updateUser = function(cb, cb_arg, req, console) {
 	var exists;
 	var userid = params.userid; 
 
-console.log (req.body);
-console.log (userid > 0);
-
 	data = {
 		UserType: 1,
 		CreationDate: '2014-04-01 13:00:00',
@@ -33,23 +30,23 @@ console.log (userid > 0);
 		Secret: params.secret,
 		Phone: params.phone
 	}
-	if (userid > 0) {
-		sql = 'select UserID from tUsers where Email = ? '	
+	if (params.email) {
+		sql = 'select UserID from tUsers where Email = ? ';
 		conn.query(sql, params.email, function(err, rows, fields) {
 			if (err) throw err;
 			if (rows) {
 				exists = true;
+			} else {
+				self.all = data;
+				cb(cb_arg);
 			};
 		});
-	} else {
-		self.all = data;
-		cb(cb_arg);
 	}
-	if (exists) {
-console.log ("here");
-			sql = 'update tUsers set ? where UserID = ?'
+
+	if (userid) {
+			sql = 'update tUsers set ? where UserID = ? '
 			console.log (sql);
-			conn.query(sql, data, userid, function(err, rows, fields) {
+			conn.query(sql, [data, userid], function(err, rows, fields) {
 				if (err) throw err;
 				self.all = rows;
 				cb(cb_arg);
